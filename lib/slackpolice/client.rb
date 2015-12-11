eeae3509a1902c33f9dd7b1e0911a932a5b45517
ISOBE Kazuhiko <muramasa64@gmail.com>
2015-12-11 13:51:08 +0900

    feat(expired_files): add expired_files method

--- lib/slackpolice/client.rb
+++ lib/slackpolice/client.rb
@@ -1,5 +1,6 @@
 require 'slack-api-wrapper'
 require 'logger'
+require 'date'
 
 module Slackpolice
   class Client
@@ -19,9 +20,30 @@ module Slackpolice
       end
     end
 
+    def delete_expired_files
+      deleted_files = []
+
+    end
+
     def no_members_channels
       @client.channels_list['channels'].select do |c|
         c['num_members'] == 0 && c['is_archived'] == false
+      end
+    end
+
+    def expired_files(ts_to = Date.today.prev_day(7))
+      page = 1
+      Enumerator.new do |y|
+        begin
+          resp = @client.files_list('ts_to' => ts_to.to_time.to_i, 'page' => page)
+          if resp['ok']
+            pages = resp['paging']['pages']
+            resp['files'].map do |file|
+              y << file
+            end
+          end
+          page += 1
+        end while pages > page || 100 > page # over 100 page is not found. api bug?
       end
     end
   end
